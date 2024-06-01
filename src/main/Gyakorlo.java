335ec0db49915db41ee82c173fede83b97cf6751
Yclaze <teralocsi@gmail.com>
2024-06-01 09:15:39 +0200

    void progtetel(), 4 for, 4 while

--- src/main/Gyakorlo.java
+++ src/main/Gyakorlo.java
@@ -7,11 +7,55 @@ public class Gyakorlo {
     }
 
     private static void feladatok() {
+        progtetelek();
         kiirKonzolra("");
     }
-
+    
+    private static int osszegzes(){
+        return 0;
+    }
+    
     private static void kiirKonzolra(String kimenet) {
         System.out.println(kimenet);
+    }
+
+    private static void progtetelek() {
+        osszegzes();
+        megszamolas();
+        minKivalasztas();
+        maxKivalasztas();
+        kivalasztas();
+        eldontesEgy();
+        eldontesMind();
+        linKereses();
+    }
+
+    private static int megszamolas() {
+        return 0;
+    }
+
+    private static int minKivalasztas() {
+        return 0;
+    }
+
+    private static int maxKivalasztas() {
+        return 0;
+    }
+    
+    private static int kivalasztas() {
+        return 0;
+    }
+
+    private static boolean eldontesEgy() {
+        return false;
+    }
+    
+    private static boolean eldontesMind() {
+        return false;
+    }
+    
+    private static int linKereses() {
+        return 0;
     }
     
 }
