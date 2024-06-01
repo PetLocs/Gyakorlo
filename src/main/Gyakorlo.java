a3c20f867fc19b567c558417bae08ddcc7933e90
Yclaze <teralocsi@gmail.com>
2024-06-01 09:03:50 +0200

    void feladatok()

--- src/main/Gyakorlo.java
+++ src/main/Gyakorlo.java
@@ -3,7 +3,11 @@ package main;
 public class Gyakorlo {
 
     public static void main(String[] args) {
-        
+        feladatok();
+    }
+
+    private static void feladatok() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
